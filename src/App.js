5789b43a19cc516407f2e300b71bf0000e2b9166
Daniel Pôrto Núñez <dani.nunez.dev@gmail.com>
2019-06-08 20:06:28 -0300

    Adição de Titulo e apresentação de dados

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AppListData extends Component{
+  render(){
+    const dataTest = [
+      {
+        name:'Visitor', number: 20000
+      },
+      {
+        name:'User', number: 1234
+      },
+      {
+        name:'Sales',number: 450
+      },
+      {
+        name:'Order', number: 320
+      }];
+
+
+    return (
+      <div className='AppDataBase'>
+        {dataTest.map((data, index) =>
+          <AppData key={data.name} name={data.name} number={data.number} color='#FF00FF'/>
+        )}
+      </div>
+    )
+  }
+}
+
+class AppData extends Component{
+  render(){
+    const name = this.props.name;
+    const number = this.props.number;
+    const bgColor = this.props.color;
+    return(
+      <div className='data'>
+          <img src='https://image.flaticon.com/icons/svg/23/23228.svg'></img>
+          <p className='data-number pData'>{number}</p>
+          <p className='data-name pData'>{name}</p>
+      </div>
+    );
+    
+  }
+}
+
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className='Page-Overview'>Page Overview</h1>
+        </header>
+        <AppListData />
+      </div>
+    );
+  }
 }
 
 export default App;
