43d897009f478dd27027121a6270420f5caadc27
Daniel Pôrto Núñez <dani.nunez.dev@gmail.com>
2019-06-10 15:52:59 -0300

    Ajuste organizacional do código

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
+import AppListData from './AppListData';
 import views from './images/eye.png';
 import user from './images/user.png';
 import sales from './images/sales.png';
 import order from './images/order.png';
 
-import './App.css';
-
-class AppListData extends Component{
-  render(){
-    const dataTest = [
+class App extends Component{
+  state = {
+    dataList: [
       {
         name:'Visitor', number: 20000, url: views
       },
@@ -20,47 +19,16 @@ class AppListData extends Component{
       },
       {
         name:'Order', number: 320, url: order
-      }];
+      }]
 
-
-    return (
-      <div className='AppDataBase'>
-        {dataTest.map((data, index) =>
-          <AppData key={data.name} name={data.name} number={data.number} url={data.url}/>
-        )}
-      </div>
-    )
   }
-}
-
-class AppData extends Component{
-  render(){
-    const name = this.props.name;
-    const number = this.props.number;
-    const url = this.props.url;
-
-    return(
-      <div className='data' id={name}>
-          <div className='data-image' style={{
-                        backgroundImage: `url(${this.props.url})`
-                    }}/>
-          <p className='data-number pData'>{number}</p>
-          <p className='data-name pData'>{name}</p>
-      </div>
-    );
-    
-  }
-}
-
-
-class App extends Component{
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <h1 className='Page-Overview'>Page Overview</h1>
         </header>
-        <AppListData />
+        <AppListData  dataList={this.state.dataList}/>
       </div>
     );
   }
---
+++ src/AppListData.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+
+
+import './App.css';
+
+function AppData(props){
+    const name = props.name;
+    const number = props.number;
+    const url = props.url;
+
+    return(
+      <div className='data' id={name}>
+          <div className='data-image' style={{
+                        backgroundImage: `url(${props.url})`
+                    }}/>
+          <p className='data-number pData'>{number}</p>
+          <p className='data-name pData'>{name}</p>
+      </div>
+    );
+    
+}
+
+function AppListData(props){
+
+    return (
+      <div className='AppDataBase'>
+        {props.dataList.map((data, index) =>
+          <AppData key={data.name} name={data.name} number={data.number} url={data.url}/>
+        )}
+      </div>
+    )
+  }
+
+
+
+export default AppListData
